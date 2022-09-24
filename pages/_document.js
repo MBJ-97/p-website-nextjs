401ff37cbfe758eddd84252f1f595331f9e2bbf2
MBJ-97 <mehdiibeldjoudi@gmail.com>
2022-09-24 19:46:07 +0100

    Google domain verification

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,10 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        <meta
+          name="google-site-verification"
+          content="61ljs1Mp2C9RZBEl4AEo65ZbdvdCMjW-ulqn9E_sP5M"
+        />
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
