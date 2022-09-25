cc61d23be3bc1a03fb958f842161ead71f240f60
MBJ-97 <mehdiibeldjoudi@gmail.com>
2022-09-25 09:32:22 +0100

    added google analytics

--- components/Layout.js
+++ components/Layout.js
@@ -1,6 +1,7 @@
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import Head from "next/head";
+import Script from "next/script";
 
 export default function Layout({ children }) {
   return (
@@ -8,6 +9,22 @@ export default function Layout({ children }) {
       <Head>
         <title>Mahdi Beldjoudi</title>
       </Head>
+      <Script
+        id="GA-scripts"
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+
+      <Script id="GA-script" strategy="lazyOnload">
+        {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                    page_path: window.location.pathname,
+                    });
+                `}
+      </Script>
       <div className="flex min-h-screen flex-col dark:bg-gray dark:text-white">
         <Navbar />
         <div className="container mx-auto mt-10 md:mt-20 w-full max-w-5xl px-3">
